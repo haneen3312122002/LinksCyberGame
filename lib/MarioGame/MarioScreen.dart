9f7a1f15ffa52399ab45e94e020b9f86a610db5b
haneen khanfar <haneenkhanfar3@yahoo.com>
2024-11-01 14:11:00 +0300

    move char

--- lib/MarioGame/MarioScreen.dart
+++ lib/MarioGame/MarioScreen.dart
@@ -9,30 +9,46 @@ class MarioGameScreen extends StatefulWidget {
 class _MarioGameScreenState extends State<MarioGameScreen> {
   double _characterXPosition =
       0; // Initial horizontal position of the character
-  double _characterYPosition = 30; // Initial vertical position of the character
+  double _characterYPosition =
+      0.05; // Initial vertical position relative to screen height
   String _characterDirection = 'TopChar.png'; // Initial character image
 
   // Function to move character left, right, forward, or backward
   void _moveCharacter(String direction) {
     setState(() {
-      if (direction == 'left') {
-        _characterXPosition -= 40; // Move left
+      double screenWidth = MediaQuery.of(context).size.width;
+      double screenHeight = MediaQuery.of(context).size.height;
+
+      // Set step sizes to match letter spacing
+      double moveDistanceX = screenWidth * 0.06; // Horizontal step size
+      double moveDistanceY = screenHeight * 0.04; // Vertical step size
+
+      // Update movement constraints
+      if (direction == 'left' && _characterXPosition > -screenWidth / 2) {
+        _characterXPosition -= moveDistanceX;
         _characterDirection = 'LeftChar.png';
-      } else if (direction == 'right') {
-        _characterXPosition += 40; // Move right
+      } else if (direction == 'right' &&
+          _characterXPosition < screenWidth / 2) {
+        _characterXPosition += moveDistanceX;
         _characterDirection = 'RightChar.png';
-      } else if (direction == 'forward') {
-        _characterYPosition += 40; // Move upward along the central street
+      } else if (direction == 'forward' &&
+          _characterYPosition < screenHeight / 2) {
+        _characterYPosition += moveDistanceY;
         _characterDirection = 'TopChar.png';
-      } else if (direction == 'backward') {
-        _characterYPosition -= 40; // Move downward along the central street
-        _characterDirection = 'DownChar.png'; // Change to downward image
+      } else if (direction == 'backward' &&
+          _characterYPosition > -screenHeight / 2) {
+        _characterYPosition -= moveDistanceY;
+        _characterDirection = 'DownChar.png';
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    double iconSize =
+        MediaQuery.of(context).size.width * 0.02; // Arrow button size
+    double buttonPadding = MediaQuery.of(context).size.width * 0.01;
+
     return Scaffold(
       body: Stack(
         children: [
@@ -43,35 +59,44 @@ class _MarioGameScreenState extends State<MarioGameScreen> {
           ),
           // Control buttons arranged in a diamond shape
           Positioned(
-            bottom: 70,
-            right: 20,
+            bottom: MediaQuery.of(context).size.height * 0.08,
+            right: MediaQuery.of(context).size.width * 0.05,
             child: Column(
               children: [
                 // Up Arrow
                 ArrowButton(
                   icon: Icons.arrow_upward,
                   onPressed: () => _moveCharacter('forward'),
+                  iconSize: iconSize,
+                  padding: buttonPadding,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     // Left Arrow
                     ArrowButton(
-                      icon: Icons.arrow_forward,
-                      onPressed: () => _moveCharacter('left'),
-                    ),
-                    SizedBox(width: 10),
-                    // Right Arrow
-                    ArrowButton(
                       icon: Icons.arrow_back,
                       onPressed: () => _moveCharacter('right'),
+                      iconSize: iconSize,
+                      padding: buttonPadding,
                     ),
+                    SizedBox(width: buttonPadding),
+                    ArrowButton(
+                      icon: Icons.arrow_forward,
+                      onPressed: () => _moveCharacter('left'),
+                      iconSize: iconSize,
+                      padding: buttonPadding,
+                    ),
+
+                    // Right Arrow
                   ],
                 ),
                 // Down Arrow
                 ArrowButton(
                   icon: Icons.arrow_downward,
                   onPressed: () => _moveCharacter('backward'),
+                  iconSize: iconSize,
+                  padding: buttonPadding,
                 ),
               ],
             ),
@@ -82,12 +107,19 @@ class _MarioGameScreenState extends State<MarioGameScreen> {
   }
 }
 
-// Widget to create a styled arrow button
+// Widget to create a styled arrow button with responsive sizing
 class ArrowButton extends StatelessWidget {
   final IconData icon;
   final VoidCallback onPressed;
+  final double iconSize;
+  final double padding;
 
-  ArrowButton({required this.icon, required this.onPressed});
+  ArrowButton({
+    required this.icon,
+    required this.onPressed,
+    required this.iconSize,
+    required this.padding,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -107,8 +139,8 @@ class ArrowButton extends StatelessWidget {
         icon: Icon(icon),
         color: Colors.white,
         onPressed: onPressed,
-        iconSize: 30, // Smaller icon size
-        padding: EdgeInsets.all(12),
+        iconSize: iconSize,
+        padding: EdgeInsets.all(padding),
       ),
     );
   }
