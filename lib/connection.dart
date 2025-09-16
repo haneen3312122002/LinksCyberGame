8f27bd48e4e7c5100085cd126155d289e84142e6
Ibrahem117 <ibrahemalnasir@gmail.com>
2025-09-16 13:53:05 +0300

    server

--- lib/connection.dart
+++ lib/connection.dart
@@ -113,7 +113,7 @@ class MarioApiService {
 
     try {
       final response = await http.post(
-        Uri.parse('http://192.168.100.35:5000/compare_word'), // Replace with your API URL
+        Uri.parse('http://192.168.209.100.35/compare_word'), // Replace with your API URL
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode(data),
       );
